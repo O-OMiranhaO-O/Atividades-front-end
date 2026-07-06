7e492a82ea3b2fc423af15c33ad2d82559f5632b
O-OMiranhaO-O <raf4elns@gmail.com>
2026-07-06 08:48:41 -0300

    Adicionando pasta Exercício 02

---
+++ Exercício 02/script.js
@@ -0,0 +1,18 @@
+function modoClaro() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+
+    document.getElementById("mensagem").innerText = "Modo claro ativado!";
+
+}
+
+
+function modoEscuro() {
+    document.body.style.backgroundColor = "#000080";
+    document.body.style.color = "white";
+
+
+    document.getElementById("mensagem").innerText = "Modo escuro ativado!";
+
+}
